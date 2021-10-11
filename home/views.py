0202fc54cc4f08b6c1fce9466ce9effd766a5ee7
Dinara <dinara080999@gmail.com>
2021-10-11 17:07:29 +0600

    home

--- home/views.py
+++ home/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from .models import *
 
 
-def index(request):
-    return render(request, 'index.html')
-
-
 def about(request):
     return render(request, 'about.html')
+
+
+def agentsingle(request):
+    return render(request, 'agent-single.html')
+
+def agentsgrid(request):
+    return render(request, 'agents-grid.html')
 
 
 def agent(request):
@@ -18,26 +21,29 @@ def blog(request):
     return render(request, 'blog-grid.html')
 
 
+def blogsingle(request):
+    return render(request, 'blog-single.html')
+
+
 def contact(request):
     return render(request, 'contact.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+
+def propertysingle(request):
+    return render(request, 'property-single.html')
 
 
 def property(request):
     return render(request, 'property.html')
 
 
-# def propertys(request):
-#     return render(request, 'propertys.html')
-
-def agentsgrid(request):
-    return render(request, 'agents-grid.html')
-
-
-def agent(request):
-    agent = Agent.objects.all()
-    return render(request, 'index.html', {'agent': agent})
-
-
 def home(request):
     home = Home.objects.all()
-    return render(request, 'property.html', {'home': home})
+    return render(request, 'property.html', 'propertysingle', {'home': home})
+
+
